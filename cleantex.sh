1f6f29811acdec32f3873fa4bbfe12a80f3cd055
Daniel Bonnéry <daniel.bonnery@gmail.com>
2018-11-01 18:24:20 -0400

    Update cleantex.sh

--- cleantex.sh
+++ cleantex.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-if hash cleantex 2>/dev/null; then echo -e "#command line for cleantex added  $(date)\n cleantex() {find ${1:-~} \( -iname '*.aux' -or -iname '.*.aux'  \) -delete}" >>~/.bashrc; else echo "command cleantex already defined"; fi;
+if hash cleantex 2>/dev/null; 
+  then 
+    echo "command cleantex already defined"; 
+  else 
+    echo -e "#command line for cleantex added  $(date)\ncleantex() {\n find \${1:-~} \( -iname '*.aux' -or -iname '.*.aux'  \) -delete\n}">>.bashrc;
+    source ~/.bashrc;
+fi;
