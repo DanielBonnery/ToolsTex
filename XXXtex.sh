a3a9403232eed119289e98e4d5c120e01bd6a55d
Daniel Bonnéry <daniel.bonnery@gmail.com>
2018-11-29 09:42:10 -0500

    Update XXXtex.sh

--- XXXtex.sh
+++ XXXtex.sh
@@ -2,6 +2,6 @@ if hash XXXtex 2>/dev/null;
   then 
     echo "command XXXtex already defined"; 
   else     
-    echo -e "#command line for XXXtex added  $(date)\nXXXtex() {\n pdflatex \"\$(basename \$1).tex\" -shell-escape -interaction=batchmode;bibtex -terse \"\$(basename \$1).aux\";pdflatex \"\$(basename \$1).tex\" -shell-escape -interaction=batchmode;\n}">>.bashrc;
+    echo -e "#command line for XXXtex added  $(date)\nXXXtex() {\n pdflatex \"\$(basename \$1).tex\" -shell-escape -interaction=batchmode;bibtex -terse \"\$(basename \$1).aux\";pdflatex \"\$(basename \$1).tex\" -shell-escape -interaction=batchmode;\n}">>~/.bashrc;
     source ~/.bashrc
 fi;
